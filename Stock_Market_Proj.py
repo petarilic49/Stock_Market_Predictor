32bb2a76c16d7515556d9633699513bb3877333e
Petar Ilic <petarilic49@gmail.com>
2021-05-05 15:01:38 -0400

    Fixed KNN to same independent X as LR (correct)

--- Stock_Market_Proj.py
+++ Stock_Market_Proj.py
@@ -1,13 +1,13 @@
 #PURPOSE: to investigate the different machine learning regression algorithms and see which is best for predicting stock market prices
 import numpy as  np
 import pandas as pd 
-import datetime
+import sklearn.metrics as sm
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
-import sklearn.metrics as sm
 from sklearn import neighbors
 from sklearn.model_selection import GridSearchCV
+from math import sqrt
 from matplotlib import pyplot as plt
 
 
@@ -59,7 +59,75 @@ data = pd.read_csv('AMD.csv')
 #         'Friday': 1
 #     }.get(d, 0)
 
-def LinearReg(tdata):
+# def LinearReg(tdata):
+#     #Load the Opening Price, Volume, and Closing Price per day into a new Dataframe
+#     new_data = pd.DataFrame({'Open' : tdata['Open'], 'Volume' : tdata['Volume'], 'Close': tdata['Close']})
+    
+#     #Split the Training and the Test data
+#     #Dont need to use the built in test_train_split because that will randomly pick points of our data but we need to use the first 70% of data for train
+#     X_train = pd.DataFrame({'Open': new_data['Open'][:round(len(new_data)*0.70)], 'Volume': new_data['Volume'][:round(len(new_data)*0.70)]})
+#     #Convert the DataFrame to a 2D array
+#     X_train = np.array(X_train[['Open','Volume']])
+#     Y_train = np.array(new_data['Close'][:round(len(new_data)*0.70)])
+
+#     X_test = pd.DataFrame({'Open': new_data['Open'][round(len(new_data)*0.70):len(new_data)], 'Volume': new_data['Volume'][round(len(new_data)*0.70):len(new_data)]})
+#     #Convert the DataFrame to a 2D array
+#     X_test = np.array(X_test[['Open','Volume']])
+#     Y_test = np.array(new_data['Close'][round(len(new_data)*0.70):len(new_data)])
+
+#     #Need to create the matrices: X = [1 X1 X2] where X1 and X2 are column vectors of the open price and volume for the training data
+#     #We have to scale the values so there is no scews
+#     scaler = MinMaxScaler(feature_range=(0,1))
+#     X_train = scaler.fit_transform(X_train)
+#     X_test = scaler.fit_transform(X_test)
+#     col1 = np.ones((len(X_train),1))
+#     col2 = pd.DataFrame({'Open': new_data['Open'][:round(len(new_data)*0.70)]}).to_numpy()
+#     col2 = scaler.fit_transform(col2)
+#     col3 = pd.DataFrame({'Volume': new_data['Volume'][:round(len(new_data)*0.70)]}).to_numpy()
+#     col3 = scaler.fit_transform(col3)
+    
+#     #Combine all three columns into a matrix, the hstack function only takes in one argument
+#     X_matrix = np.hstack((col1, col2, col3))
+#     Y_matrix = Y_train[:, np.newaxis]
+    
+#     #Need to perform matrix algebra to get B values: B_vals = (X_transpose * X)^-1 * X_transpose*Y
+#     X_transpose = X_matrix.transpose()
+#     #Matrix multiplication is done with .dot() function in python since * operand would just perform element multiplication
+#     firstop = np.linalg.inv(X_transpose.dot(X_matrix))
+#     secondop = X_transpose.dot(Y_matrix)
+#     B_vals = firstop.dot(secondop)
+
+#     #The Linear Regression model looks as follows: Y_hat = B_not + B_1*X1 + B_2*X2
+#     for i in range(round(len(new_data)*0.7), len(new_data)):
+#         new_data.at[i,'Predicted Close'] = B_vals[0] + (B_vals[1]*X_test[i-176][0]) + (B_vals[2]*X_test[i-176][1])
+
+
+#     #Use Built in scikit Linear Regression model
+#     model = LinearRegression()
+#     model.fit(X_train, Y_train)
+#     prediction = model.predict(X_test)
+
+#     for i in range(round(len(new_data)*0.7), len(new_data)):
+#          new_data.at[i,'Predicted Close Model'] = prediction[i-176]
+
+#     plt.plot(tdata['Date'], new_data['Close'], label = 'Real Prices')
+#     plt.plot(tdata['Date'], new_data['Predicted Close'], label = 'Predicted Prices')
+#     plt.plot(tdata['Date'], new_data['Predicted Close Model'], label = 'Predicted Model Prices')
+#     plt.xlabel('Date')
+#     plt.ylabel('Closing Price')
+#     plt.legend()
+#     plt.show()
+
+#     # Calculate the error of the model. Since both the created and the scikit models output the same value we will just use the scikit model to find accuracy
+#     print("Mean absolute error =", round(sm.mean_absolute_error(Y_test, prediction), 2)) 
+#     print("Mean squared error =", round(sm.mean_squared_error(Y_test, prediction), 2)) 
+#     print("Median absolute error =", round(sm.median_absolute_error(Y_test, prediction), 2)) 
+#     print("Explain variance score =", round(sm.explained_variance_score(Y_test, prediction), 2)) 
+#     print("R2 score =", round(sm.r2_score(Y_test, prediction), 2))
+
+#K-Nearest Neighbour Method
+#The same independent variable assumptions will be made for KNN method 
+def Knearest(tdata):
     #Load the Opening Price, Volume, and Closing Price per day into a new Dataframe
     new_data = pd.DataFrame({'Open' : tdata['Open'], 'Volume' : tdata['Volume'], 'Close': tdata['Close']})
     
@@ -75,44 +143,42 @@ def LinearReg(tdata):
     X_test = np.array(X_test[['Open','Volume']])
     Y_test = np.array(new_data['Close'][round(len(new_data)*0.70):len(new_data)])
 
-    #Need to create the matrices: X = [1 X1 X2] where X1 and X2 are column vectors of the open price and volume for the training data
-    #We have to scale the values so there is no scews
+    #Need to scale the X data so that we dont have the volume scew the data 
     scaler = MinMaxScaler(feature_range=(0,1))
     X_train = scaler.fit_transform(X_train)
     X_test = scaler.fit_transform(X_test)
-    col1 = np.ones((len(X_train),1))
-    col2 = pd.DataFrame({'Open': new_data['Open'][:round(len(new_data)*0.70)]}).to_numpy()
-    col2 = scaler.fit_transform(col2)
-    col3 = pd.DataFrame({'Volume': new_data['Volume'][:round(len(new_data)*0.70)]}).to_numpy()
-    col3 = scaler.fit_transform(col3)
+
+    #Need to find the optimal K value. This can be done by evaluating the root mean square error for a series of K values chosen
+    # rms_vals = []
+    # for K in range(20):
+    #     K = K+1
+    #     model = neighbors.KNeighborsRegressor(n_neighbors=K)
+    #     model.fit(X_train, Y_train)
+    #     prediction = model.predict(X_test)
+
+    #     err = sqrt(sm.mean_squared_error(Y_test, prediction))
+    #     rms_vals.append(err)
+    #     print('The RMSE for K= ',K,'is: ',err)
+
+    # #We want K value with minimum RMSE error value which is approximately 9 or 2 according to the plot created below. There is a better way to find this out
+    # plt.plot(rms_vals)
+    # plt.show()
+
+    k_vals = {'n_neighbors': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+
+    knn = neighbors.KNeighborsRegressor()
+
+    model = GridSearchCV(knn, k_vals, cv=5)
+    model.fit(X_train,Y_train)
+    print(model.best_params_)
     
-    #Combine all three columns into a matrix, the hstack function only takes in one argument
-    X_matrix = np.hstack((col1, col2, col3))
-    Y_matrix = Y_train[:, np.newaxis]
-    
-    #Need to perform matrix algebra to get B values: B_vals = (X_transpose * X)^-1 * X_transpose*Y
-    X_transpose = X_matrix.transpose()
-    #Matrix multiplication is done with .dot() function in python since * operand would just perform element multiplication
-    firstop = np.linalg.inv(X_transpose.dot(X_matrix))
-    secondop = X_transpose.dot(Y_matrix)
-    B_vals = firstop.dot(secondop)
-
-    #The Linear Regression model looks as follows: Y_hat = B_not + B_1*X1 + B_2*X2
-    for i in range(round(len(new_data)*0.7), len(new_data)):
-        new_data.at[i,'Predicted Close'] = B_vals[0] + (B_vals[1]*X_test[i-176][0]) + (B_vals[2]*X_test[i-176][1])
-
-
-    #Use Built in scikit Linear Regression model
-    model = LinearRegression()
-    model.fit(X_train, Y_train)
     prediction = model.predict(X_test)
-
+    
     for i in range(round(len(new_data)*0.7), len(new_data)):
-         new_data.at[i,'Predicted Close Model'] = prediction[i-176]
+        new_data.at[i,'Predicted Close'] = prediction[i-176]
 
     plt.plot(tdata['Date'], new_data['Close'], label = 'Real Prices')
     plt.plot(tdata['Date'], new_data['Predicted Close'], label = 'Predicted Prices')
-    plt.plot(tdata['Date'], new_data['Predicted Close Model'], label = 'Predicted Model Prices')
     plt.xlabel('Date')
     plt.ylabel('Closing Price')
     plt.legend()
@@ -125,48 +191,7 @@ def LinearReg(tdata):
     print("Explain variance score =", round(sm.explained_variance_score(Y_test, prediction), 2)) 
     print("R2 score =", round(sm.r2_score(Y_test, prediction), 2))
 
-#K-Nearest Neighbour Method
-# def Knearest(tdata):
-#     new_data = pd.DataFrame({'Date' : pd.to_datetime(tdata['Date']), 'Adj Close' : tdata['Adj Close']})
-#     new_data['DayofWeek'] = new_data['Date'].dt.day_name()
-#     for i in range(0, len(new_data)):
-#         new_data.at[i, 'DayScale'] = switch_func(new_data['DayofWeek'][i])
-
-#     #Dont need to scale it because the X_train and X_test are already between 0 and 1 and it only has one variable contributing to finding price
-#     X_train = new_data['DayScale'][:round(len(new_data)*0.70)]
-#     Y_train = new_data['Adj Close'][:round(len(new_data)*0.70)]
-#     X_test = new_data['DayScale'][round(len(new_data)*0.70):len(new_data)]
-#     Y_test = new_data['Adj Close'][round(len(new_data)*0.70):len(new_data)]
-
-#     scaler = MinMaxScaler(feature_range=(0,1))
-#     x_train = np.array(X_train)
-#     x_train = x_train[:,np.newaxis]
-#     x_test = np.array(X_test)
-#     x_test = x_test[:,np.newaxis]
-#     x_train_scaled = scaler.fit_transform(x_train)
-#     x_test_scaled = scaler.fit_transform(x_test)
-
-#     k_vals = {'n_neighbors': [2, 3, 4, 5, 6, 7, 8, 9]}
-
-#     knn = neighbors.KNeighborsRegressor()
-
-#     model = GridSearchCV(knn, k_vals, cv=5)
-#     model.fit(x_train_scaled,Y_train)
-    
-#     prediction = model.predict(x_test_scaled)
-    
-#     for i in range(round(len(new_data)*0.7), len(new_data)):
-#         new_data.at[i,'Predicted Close'] = prediction[i-176]
-
-#     plt.plot(new_data['Date'], new_data['Adj Close'], label = 'Real Prices')
-#     plt.plot(new_data['Date'], new_data['Predicted Close'], label = 'Predicted Prices')
-#     plt.xlabel('Date')
-#     plt.ylabel('Closing Price')
-#     plt.legend()
-#     plt.show()
-
-
 #MovingAvg(data)     
-LinearReg(data)
-#Knearest(data)
+#LinearReg(data)
+Knearest(data)
 
