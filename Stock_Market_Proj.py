68bbcd601885f8aeebdf9f2dd4131c9b3799dbfc
Petar Ilic <petarilic49@gmail.com>
2021-05-04 21:07:33 -0400

    Implemented first draft of K nearest neighbors

--- Stock_Market_Proj.py
+++ Stock_Market_Proj.py
@@ -4,7 +4,12 @@ import pandas as pd
 import datetime
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from sklearn.metrics import mean_squared_error
+from sklearn import neighbors
+from sklearn.model_selection import GridSearchCV
 from matplotlib import pyplot as plt
+
 
 #We will use AMD stock prices for example
 #Load the stock prices and other variables into the data variable by reading the .csv file
@@ -45,70 +50,65 @@ data = pd.read_csv('AMD.csv')
 
 def switch_func(d):
     return{
-        'Monday': 0.2,
-        'Tuesday': 0.4,
-        'Wednesday': 0.6,
-        'Thursday': 0.8,
+        'Monday': 1,
+        'Tuesday': 0,
+        'Wednesday': 0,
+        'Thursday': 0,
         'Friday': 1
     }.get(d, 0)
 
-def LinearReg(tdata):
-    new_data = pd.DataFrame({'Date' : pd.to_datetime(tdata['Date']), 'Adj Close' : tdata['Adj Close']})
-    new_data['DayofWeek'] = new_data['Date'].dt.day_name()
-    for i in range(0, len(new_data)):
-        new_data.at[i, 'DayScale'] = switch_func(new_data['DayofWeek'][i])
+# def LinearReg(tdata):
+#     new_data = pd.DataFrame({'Date' : pd.to_datetime(tdata['Date']), 'Adj Close' : tdata['Adj Close']})
+#     new_data['DayofWeek'] = new_data['Date'].dt.day_name()
+#     for i in range(0, len(new_data)):
+#         new_data.at[i, 'DayScale'] = switch_func(new_data['DayofWeek'][i])
 
-    X_train = new_data['DayScale'][:round(len(new_data)*0.70)]
-    Y_train = new_data['Adj Close'][:round(len(new_data)*0.70)]
-    X_test = new_data['DayScale'][round(len(new_data)*0.70):len(new_data)]
-    Y_test = new_data['Adj Close'][round(len(new_data)*0.70):len(new_data)]
+#     X_train = new_data['DayScale'][:round(len(new_data)*0.70)]
+#     Y_train = new_data['Adj Close'][:round(len(new_data)*0.70)]
+#     X_test = new_data['DayScale'][round(len(new_data)*0.70):len(new_data)]
+#     Y_test = new_data['Adj Close'][round(len(new_data)*0.70):len(new_data)]
 
-    #Y_hat = B_not + B_1*X
-    Y_mean = 0
-    X_mean = 0
-    for i in range (0, len(Y_train)):
-        Y_mean = Y_mean + Y_train.at[i]
-        X_mean = X_mean + X_train.at[i]
-    Y_mean = Y_mean / len(Y_train)
-    X_mean = X_mean / len(X_train)
+#     #Y_hat = B_not + B_1*X
+#     Y_mean = Y_train.mean()
+#     X_mean = X_train.mean()
 
-    #Calculating B_1
-    B_num = 0
-    B_den = 0
-    for i in range(0,len(X_train)):
-        B_num = B_num + (X_train.at[i]*Y_train.at[i] - Y_mean*X_train.at[i])
-        B_den = B_den + (pow(X_train.at[i],2) - X_mean*X_train.at[i])
-    B_one = B_num / B_den
+#     #Calculating B_1
+#     B_num = 0
+#     B_den = 0
+#     for i in range(0,len(X_train)):
+#         B_num = B_num + (X_train.at[i]*Y_train.at[i] - Y_mean*X_train.at[i])
+#         B_den = B_den + (pow(X_train.at[i],2) - X_mean*X_train.at[i])
+#     B_one = B_num / B_den
     
 
-    #Calculating B_not
-    B_not = Y_mean - B_one*X_mean
+#     #Calculating B_not
+#     B_not = Y_mean - B_one*X_mean
     
-    #Create the Linear Regression Model
-    for i in range(round(len(new_data)*0.7), len(new_data)):
-        new_data.at[i,'Predicted Close'] = B_not + B_not*new_data.at[i,'DayScale']
+#     #Create the Linear Regression Model
+#     for i in range(round(len(new_data)*0.7), len(new_data)):
+#         new_data.at[i,'Predicted Close'] = B_not + B_not*new_data.at[i,'DayScale']
     
 
-    model = LinearRegression()
-    xtrain = np.array(X_train)
-    xtrain = xtrain[:, np.newaxis]
-    xtest = np.array(X_test)
-    xtest = xtest[:, np.newaxis]
+#     model = LinearRegression()
+#     xtrain = np.array(X_train)
+#     xtrain = xtrain[:, np.newaxis]
+#     xtest = np.array(X_test)
+#     xtest = xtest[:, np.newaxis]
 
-    model.fit(xtrain, Y_train)
-    prediction = model.predict(xtest)
-    #print(prediction)
+#     model.fit(xtrain, Y_train)
+#     prediction = model.predict(xtest)
+#     #print(prediction)
 
-    for i in range(round(len(new_data)*0.7), len(new_data)):
-        new_data.at[i,'Predicted Close Model'] = prediction[i-176]
+#     for i in range(round(len(new_data)*0.7), len(new_data)):
+#         new_data.at[i,'Predicted Close Model'] = prediction[i-176]
 
-    plt.plot(new_data['Date'], new_data['Adj Close'], label = 'Real Prices')
-    plt.plot(new_data['Date'], new_data['Predicted Close'], label = 'Predicted Prices')
-    plt.plot(new_data['Date'], new_data['Predicted Close Model'], label = 'Predicted Model Prices')
-    plt.xlabel('Date')
-    plt.ylabel('Closing Price')
-    plt.legend()
-    plt.show()
+#     plt.plot(new_data['Date'], new_data['Adj Close'], label = 'Real Prices')
+#     plt.plot(new_data['Date'], new_data['Predicted Close'], label = 'Predicted Prices')
+#     plt.plot(new_data['Date'], new_data['Predicted Close Model'], label = 'Predicted Model Prices')
+#     plt.xlabel('Date')
+#     plt.ylabel('Closing Price')
+#     plt.legend()
+#     plt.show()
   
 
     #Calculate the error of the model
@@ -121,7 +121,47 @@ def LinearReg(tdata):
 
 
 #K-Nearest Neighbour Method
+def Knearest(tdata):
+    new_data = pd.DataFrame({'Date' : pd.to_datetime(tdata['Date']), 'Adj Close' : tdata['Adj Close']})
+    new_data['DayofWeek'] = new_data['Date'].dt.day_name()
+    for i in range(0, len(new_data)):
+        new_data.at[i, 'DayScale'] = switch_func(new_data['DayofWeek'][i])
+
+    #Dont need to scale it because the X_train and X_test are already between 0 and 1 and it only has one variable contributing to finding price
+    X_train = new_data['DayScale'][:round(len(new_data)*0.70)]
+    Y_train = new_data['Adj Close'][:round(len(new_data)*0.70)]
+    X_test = new_data['DayScale'][round(len(new_data)*0.70):len(new_data)]
+    Y_test = new_data['Adj Close'][round(len(new_data)*0.70):len(new_data)]
+
+    scaler = MinMaxScaler(feature_range=(0,1))
+    x_train = np.array(X_train)
+    x_train = x_train[:,np.newaxis]
+    x_test = np.array(X_test)
+    x_test = x_test[:,np.newaxis]
+    x_train_scaled = scaler.fit_transform(x_train)
+    x_test_scaled = scaler.fit_transform(x_test)
+
+    k_vals = {'n_neighbors': [2, 3, 4, 5, 6, 7, 8, 9]}
+
+    knn = neighbors.KNeighborsRegressor()
+
+    model = GridSearchCV(knn, k_vals, cv=5)
+    model.fit(x_train_scaled,Y_train)
+    
+    prediction = model.predict(x_test_scaled)
+    
+    for i in range(round(len(new_data)*0.7), len(new_data)):
+        new_data.at[i,'Predicted Close'] = prediction[i-176]
+
+    plt.plot(new_data['Date'], new_data['Adj Close'], label = 'Real Prices')
+    plt.plot(new_data['Date'], new_data['Predicted Close'], label = 'Predicted Prices')
+    plt.xlabel('Date')
+    plt.ylabel('Closing Price')
+    plt.legend()
+    plt.show()
+
 
 #MovingAvg(data)     
-LinearReg(data)
+#LinearReg(data)
+Knearest(data)
 
