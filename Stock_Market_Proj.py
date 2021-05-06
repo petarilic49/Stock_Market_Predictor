2ecdc79f41e764e055320363c5e5f8c6b17eb9b3
Petar Ilic <petarilic49@gmail.com>
2021-05-06 14:27:15 -0400

    First Draft of AutoARIMA

--- Stock_Market_Proj.py
+++ Stock_Market_Proj.py
@@ -9,7 +9,10 @@ from sklearn import neighbors
 from sklearn.model_selection import GridSearchCV
 from math import sqrt
 from matplotlib import pyplot as plt
-
+from statsmodels.tsa.stattools import adfuller
+from statsmodels.graphics.tsaplots import plot_acf, plot_pacf
+from statsmodels.tsa.arima.model import ARIMA
+from pmdarima.arima import auto_arima
 
 #We will use AMD stock prices for example
 #Load the stock prices and other variables into the data variable by reading the .csv file
@@ -127,56 +130,128 @@ data = pd.read_csv('AMD.csv')
 
 #K-Nearest Neighbour Method
 #The same independent variable assumptions will be made for KNN method 
-def Knearest(tdata):
-    #Load the Opening Price, Volume, and Closing Price per day into a new Dataframe
-    new_data = pd.DataFrame({'Open' : tdata['Open'], 'Volume' : tdata['Volume'], 'Close': tdata['Close']})
+# def Knearest(tdata):
+#     #Load the Opening Price, Volume, and Closing Price per day into a new Dataframe
+#     new_data = pd.DataFrame({'Open' : tdata['Open'], 'Volume' : tdata['Volume'], 'Close': tdata['Close']})
     
-    #Split the Training and the Test data
-    #Dont need to use the built in test_train_split because that will randomly pick points of our data but we need to use the first 70% of data for train
-    X_train = pd.DataFrame({'Open': new_data['Open'][:round(len(new_data)*0.70)], 'Volume': new_data['Volume'][:round(len(new_data)*0.70)]})
-    #Convert the DataFrame to a 2D array
-    X_train = np.array(X_train[['Open','Volume']])
+#     #Split the Training and the Test data
+#     #Dont need to use the built in test_train_split because that will randomly pick points of our data but we need to use the first 70% of data for train
+#     X_train = pd.DataFrame({'Open': new_data['Open'][:round(len(new_data)*0.70)], 'Volume': new_data['Volume'][:round(len(new_data)*0.70)]})
+#     #Convert the DataFrame to a 2D array
+#     X_train = np.array(X_train[['Open','Volume']])
+#     Y_train = np.array(new_data['Close'][:round(len(new_data)*0.70)])
+
+#     X_test = pd.DataFrame({'Open': new_data['Open'][round(len(new_data)*0.70):len(new_data)], 'Volume': new_data['Volume'][round(len(new_data)*0.70):len(new_data)]})
+#     #Convert the DataFrame to a 2D array
+#     X_test = np.array(X_test[['Open','Volume']])
+#     Y_test = np.array(new_data['Close'][round(len(new_data)*0.70):len(new_data)])
+
+#     #Need to scale the X data so that we dont have the volume scew the data 
+#     scaler = MinMaxScaler(feature_range=(0,1))
+#     X_train = scaler.fit_transform(X_train)
+#     X_test = scaler.fit_transform(X_test)
+
+#     #Need to find the optimal K value. This can be done by evaluating the root mean square error for a series of K values chosen
+#     # rms_vals = []
+#     # for K in range(20):
+#     #     K = K+1
+#     #     model = neighbors.KNeighborsRegressor(n_neighbors=K)
+#     #     model.fit(X_train, Y_train)
+#     #     prediction = model.predict(X_test)
+
+#     #     err = sqrt(sm.mean_squared_error(Y_test, prediction))
+#     #     rms_vals.append(err)
+#     #     print('The RMSE for K= ',K,'is: ',err)
+
+#     # #We want K value with minimum RMSE error value which is approximately 9 or 2 according to the plot created below. There is a better way to find this out
+#     # plt.plot(rms_vals)
+#     # plt.show()
+
+#     k_vals = {'n_neighbors': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+
+#     knn = neighbors.KNeighborsRegressor()
+
+#     model = GridSearchCV(knn, k_vals, cv=5)
+#     model.fit(X_train,Y_train)
+#     print(model.best_params_)
+    
+#     prediction = model.predict(X_test)
+    
+#     for i in range(round(len(new_data)*0.7), len(new_data)):
+#         new_data.at[i,'Predicted Close'] = prediction[i-176]
+
+#     plt.plot(tdata['Date'], new_data['Close'], label = 'Real Prices')
+#     plt.plot(tdata['Date'], new_data['Predicted Close'], label = 'Predicted Prices')
+#     plt.xlabel('Date')
+#     plt.ylabel('Closing Price')
+#     plt.legend()
+#     plt.show()
+
+#     # Calculate the error of the model. Since both the created and the scikit models output the same value we will just use the scikit model to find accuracy
+#     print("Mean absolute error =", round(sm.mean_absolute_error(Y_test, prediction), 2)) 
+#     print("Mean squared error =", round(sm.mean_squared_error(Y_test, prediction), 2)) 
+#     print("Median absolute error =", round(sm.median_absolute_error(Y_test, prediction), 2)) 
+#     print("Explain variance score =", round(sm.explained_variance_score(Y_test, prediction), 2)) 
+#     print("R2 score =", round(sm.r2_score(Y_test, prediction), 2))
+
+#Auto ARIMA Method
+def AutoARIMA(tdata):
+    #Load the data into a new variable
+    #Since we are only looking at previous close prices, this is a univariate model itself
+    new_data = pd.DataFrame({'Close': tdata['Close']})
     Y_train = np.array(new_data['Close'][:round(len(new_data)*0.70)])
-
-    X_test = pd.DataFrame({'Open': new_data['Open'][round(len(new_data)*0.70):len(new_data)], 'Volume': new_data['Volume'][round(len(new_data)*0.70):len(new_data)]})
-    #Convert the DataFrame to a 2D array
-    X_test = np.array(X_test[['Open','Volume']])
+    Y_train = Y_train[:, np.newaxis]
     Y_test = np.array(new_data['Close'][round(len(new_data)*0.70):len(new_data)])
+    Y_test = Y_test[:, np.newaxis]
 
-    #Need to scale the X data so that we dont have the volume scew the data 
-    scaler = MinMaxScaler(feature_range=(0,1))
-    X_train = scaler.fit_transform(X_train)
-    X_test = scaler.fit_transform(X_test)
 
-    #Need to find the optimal K value. This can be done by evaluating the root mean square error for a series of K values chosen
-    # rms_vals = []
-    # for K in range(20):
-    #     K = K+1
-    #     model = neighbors.KNeighborsRegressor(n_neighbors=K)
-    #     model.fit(X_train, Y_train)
-    #     prediction = model.predict(X_test)
+    #We need to check if the time series is stationary. Just by looking at the stock prices we know its not but for good practice I'll implement the code
+    #I will use the augmented dickey fuller test to check if the data is stationary 
+    stat_test = adfuller(Y_train, autolag="AIC")
+    #print(stat_test[1]) #The test shows that our p value is 0.723 which is much greater than 0.05. This means that we have 0.723 chance 
+    #probability that null hypothesis will not be rejected and therefore we know our time series is NOT stationary
 
-    #     err = sqrt(sm.mean_squared_error(Y_test, prediction))
-    #     rms_vals.append(err)
-    #     print('The RMSE for K= ',K,'is: ',err)
+    #We now need to make our time series stationary
+    #We will use the simplest method which is just differencing the time series (ie subtract the current value by the previous)
+    new_data_stationary = new_data['Close'][:round(len(new_data)*0.70)].diff().dropna()
+    Y_train_st = np.array(new_data_stationary)
+    Y_train_st = Y_train[:, np.newaxis]
 
-    # #We want K value with minimum RMSE error value which is approximately 9 or 2 according to the plot created below. There is a better way to find this out
-    # plt.plot(rms_vals)
+    #Check again if the time series is stationary
+    stat_test = adfuller(Y_train_st, autolag="AIC")
+    #print(stat_test[1]) # The p value is much smaller than 0.05 therefore we can reject the Null Hypothesis and therefore the time series is now stationary
+    #Plot the stationarized time series
+    # plt.plot(new_data_stationary, label = 'Difference')
+    # plt.xlabel('Time')
+    # plt.ylabel('Diff')
+    # plt.legend()
     # plt.show()
 
-    k_vals = {'n_neighbors': [2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]}
+    #Need to find the p and q values for the ARIMA model. We can do this by ploting the ACF and PACF 
+    #To learn the theory of the model I will create the plots and try to pick appropriate p and q values, but in reality there is an auto_arima function that would do 
+    #so automatically 
+    #Rule for Choosing p: If the PACF of the differenced series displays a sharp cutoff and/or the lag-1 autocorrelation is positive--i.e., if the series 
+    #appears slightly "underdifferenced"--then consider adding an AR term to the model. The lag at which the PACF cuts off is the indicated number of AR terms.
+    #Rule for Choosing q: If the ACF of the differenced series displays a sharp cutoff and/or the lag-1 autocorrelation is negative--i.e., if the series 
+    #appears slightly "overdifferenced"--then consider adding an MA term to the model. The lag at which the ACF cuts off is the indicated number of MA terms.
+    #Use the following link as reference: http://people.duke.edu/~rnau/411arim3.htm
 
-    knn = neighbors.KNeighborsRegressor()
+    #Plot the ACF
+    #plot_acf(Y_train)
+    #plt.show() #q = 1
 
-    model = GridSearchCV(knn, k_vals, cv=5)
-    model.fit(X_train,Y_train)
-    print(model.best_params_)
-    
-    prediction = model.predict(X_test)
+    #Plot the PACF
+    #plot_pacf(Y_train)
+    #plt.show() #p = 0
+    auto_model = auto_arima(Y_train, start_p=1, start_q=1, test='adf', max_p=5, max_q=5, d=None, seasonal=False) # See what auto_arima tells us the p, q, and d values should be
+    print(auto_model.summary()) # Tells us they should be as follows: p = 0, d = 1, q = 1 
+    start = len(Y_train)
+    end = len(Y_train) + len(Y_test)
+    prediction = auto_model.predict(n_periods=(end-start))
     
     for i in range(round(len(new_data)*0.7), len(new_data)):
         new_data.at[i,'Predicted Close'] = prediction[i-176]
-
+    
     plt.plot(tdata['Date'], new_data['Close'], label = 'Real Prices')
     plt.plot(tdata['Date'], new_data['Predicted Close'], label = 'Predicted Prices')
     plt.xlabel('Date')
@@ -191,7 +266,8 @@ def Knearest(tdata):
     print("Explain variance score =", round(sm.explained_variance_score(Y_test, prediction), 2)) 
     print("R2 score =", round(sm.r2_score(Y_test, prediction), 2))
 
+    
 #MovingAvg(data)     
 #LinearReg(data)
-Knearest(data)
-
+#Knearest(data)
+AutoARIMA(data)
